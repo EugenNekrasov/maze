5b541d470841622cb59a0d7e017362b0bf118d9b
Eugen Nekrasov <e.nekrasov91@gmail.com>
2021-10-17 16:58:31 +0300

    main page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Maze</title>
+</svelte:head>
+
+<div class="links">
+	<a href="login">Login</a>
+	<a href="register">Register</a>
+</div>
+
+<style>
+	.links {
+		display: flex;
+	}
+
+	a {
+		color: var(--main-color);
+		font-size: 2em;
+		letter-spacing: 0.3em;
+		text-align: center;
+		text-decoration: none;
+		text-shadow: var(--shadow);
+		transition:
+		letter-spacing 0.4s ease,
+		text-shadow 0.4s ease;
+		width: 50%;
+	}
+
+	a:hover {
+		letter-spacing: normal;
+		text-shadow: none;
+	}
+</style>
