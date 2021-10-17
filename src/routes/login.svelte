6c5344428d749ab141a0628f3eec8923aac0ca15
Eugen Nekrasov <e.nekrasov91@gmail.com>
2021-10-17 17:41:32 +0300

    Login and register template components

---
+++ src/routes/login.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+	<title>Login</title>
+</svelte:head>
+
+<h2>Login</h2>
+<form>
+	<label>
+		Login
+		<input type="text" name="login">
+	</label>
+	<label>
+		Password
+		<input type="password" name="password">
+	</label>
+	<input type="submit" value="Login">
+</form>
+
+<style>
+	h2 {
+		color: var(--main-color);
+	}
+</style>
---
+++ src/routes/register.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Welcome to Maze</title>
+</svelte:head>
+
+<h2>Register</h2>
+<form>
+	<label>
+		Login
+		<input type="text" name="login">
+	</label>
+	<label>
+		Password
+		<input type="password" name="password">
+	</label>
+	<input type="submit" value="Login">
+</form>
